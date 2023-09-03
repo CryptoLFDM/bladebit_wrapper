d6cd9206a5646278d82723435e859cd33784cc02
Maxxroot <maxxroot@laposte.net>
2023-09-03 13:08:26 +0200

    fix affichage log fix db_lock fix reset status in_progress au boot

--- disk_copy.py
+++ disk_copy.py
@@ -14,21 +14,23 @@ cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS plots(plot_name TEXT UNIQUE, source TEXT, dest TEXT, status TEXT)")
 
 
+def get_plot_by_name(plot_name: str):
+    query = "SELECT * FROM plots WHERE plot_name=?"
+    cur.execute(query, (plot_name,))
+    return cur.fetchall()
+
+
 def insert_new_plot(plot_name: str, source: str):
+    if get_plot_by_name(plot_name) is not []:
+        return
     query = "INSERT INTO plots(plot_name, source, dest, status) VALUES (?, ?, null, null)"
     values = (plot_name, source)
     try:
         cur.execute(query, values)
         con.commit()
         bladebit_manager_logger.log(SUCCESS, 'plot {} added into plot manager'.format(plot_name))
-    except sqlite3.IntegrityError:
-        bladebit_manager_logger.log(FAILED, 'plot {} already exists in plot manager'.format(plot_name))
-
-
-def get_plot_by_name(plot_name: str):
-    query = "SELECT * FROM plots WHERE plot_name=?"
-    cur.execute(query, (plot_name,))
-    return cur.fetchall()
+    except sqlite3.Error as e:
+        bladebit_manager_logger.log(FAILED, 'Error occurred while inserting plot {}: {}'.format(plot_name, str(e)))
 
 
 def get_plot_status_by_name(plot_name: str):
@@ -54,8 +56,11 @@ def scan_plots():
     for staging_dir in config['staging_directories']:
         for filename in os.listdir(staging_dir):
             if filename.endswith('.plot'):
-
                 insert_new_plot(filename, staging_dir)
+
+def reset_in_progess_plots_boot():
+    cur.execute("UPDATE plots SET status=NULL WHERE status='in_progress'")
+    con.commit()
 
 
 def process_plots(destination):
@@ -71,19 +76,21 @@ def process_plots(destination):
                 source_path = os.path.join(source, plot_name)
                 dest_path = os.path.join(destination, plot_name)
                 shutil.move(source_path, dest_path)
-                bladebit_manager_logger.log(INFO, 'Moved {} from {} to {}'.format(plot_name, source, dest_path))
-                bladebit_manager_logger.log(WARNING, get_plot_status_by_name(plot_name))
-                update_plot_by_name(plot_name, destination, 'done')
-                bladebit_manager_logger.log(WARNING, get_plot_status_by_name(plot_name))
+                with multiprocessing.Lock():
+                    bladebit_manager_logger.log(INFO, 'Moved {} from {} to {}'.format(plot_name, source, dest_path))
+                    bladebit_manager_logger.log(WARNING, get_plot_status_by_name(plot_name))
+                    update_plot_by_name(plot_name, destination, 'done')
+                    bladebit_manager_logger.log(WARNING, get_plot_status_by_name(plot_name))
             else:
-                bladebit_manager_logger.log(FAILED, 'Sleep mode for 10 minutes')
-                time.sleep(600)
+                bladebit_manager_logger.log(FAILED, 'Sleep mode for 5 minutes')
+                time.sleep(300)
         except Exception as e:
             bladebit_manager_logger.log(FAILED, str(e))
-            time.sleep(600)
+            time.sleep(300)
 
 
 def start_copy():
+    reset_in_progess_plots_boot()
     bladebit_manager_logger.log(INFO, "Going to start plot manager")
     dest_dir = config['directories_to_plot']
     num_process = 5
@@ -95,5 +102,10 @@ def start_copy():
             process = multiprocessing.Process(target=process_plots, args=(destination,))
             processes.append(process)
             process.start()
+
+    try:
+        for process in processes:
+            process.join()
+    except KeyboardInterrupt:
         for process in processes:
             process.join()

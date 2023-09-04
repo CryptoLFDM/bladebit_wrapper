46ded9df21748cb60e1840b01a63c234893d21d5
Moz <contact@ether-source.fr>
2023-09-04 20:23:09 +0200

    fix debug and init way to split log into file

--- log_utils.py
+++ log_utils.py
@@ -8,23 +8,20 @@ class LoggerUtils:
     DEBUG = 1
     INFO = 2
     SUCCESS = 3
-    HIGHLIGHT = 4
-    WARNING = 5
-    FAILED = 6
+    WARNING = 4
+    FAILED = 5
 
     def __init__(self, debug: bool):
         logging.addLevelName(self.DEBUG, 'DEBUG')
         logging.addLevelName(self.INFO, 'INFO')
         logging.addLevelName(self.SUCCESS, 'SUCCESS')
-        logging.addLevelName(self.HIGHLIGHT, 'HIGHLIGHT')
         logging.addLevelName(self.WARNING, 'WARNING')
         logging.addLevelName(self.FAILED, 'FAILED')
         color_mapping = {
-            'DEBUG': 'orange',
+            'DEBUG': 'yellow',
             'DEFAULT': 'grey',
             'INFO': 'cyan',
             'SUCCESS': 'green',
-            'HIGHLIGHT': 'yellow',
             'WARNING': 'purple',
             'FAILED': 'red'
         }
@@ -36,19 +33,25 @@ class LoggerUtils:
             level = self.INFO
 
         self.formatter = colorlog.ColoredFormatter(log_pattern, log_colors=color_mapping)
-        self.handler = logging.StreamHandler()
-        self.handler.setFormatter(self.formatter)
 
         self.bladebit_plotter_logger = logging.getLogger('bladebit.wrapper.plotter')
-        self.bladebit_plotter_logger.addHandler(self.handler)
+        self.bladebit_plotter_logger_handler = logging.StreamHandler()
+        #self.bladebit_plotter_logger_handler = logging.FileHandler('plotter.log', delay=False)
+        self.bladebit_plotter_logger_handler.setFormatter(self.formatter)
+        self.bladebit_plotter_logger.addHandler(self.bladebit_plotter_logger_handler)
         self.bladebit_plotter_logger.setLevel(level)
 
         self.bladebit_manager_logger = logging.getLogger('bladebit.wrapper.manager')
-        self.bladebit_manager_logger.addHandler(self.handler)
+        self.bladebit_manager_logger_handler = logging.StreamHandler()
+        #self.bladebit_manager_logger_handler = logging.FileHandler('manager.log', delay=False)
+        self.bladebit_manager_logger_handler.setFormatter(self.formatter)
+        self.bladebit_manager_logger.addHandler(self.bladebit_plotter_logger_handler)
         self.bladebit_manager_logger.setLevel(level)
 
         self.bladebit_wrapper_logger = logging.getLogger('bladebit.wrapper.main')
-        self.bladebit_wrapper_logger.addHandler(self.handler)
+        self.bladebit_wrapper_logger_handler = logging.StreamHandler()
+        self.bladebit_wrapper_logger_handler.setFormatter(self.formatter)
+        self.bladebit_wrapper_logger.addHandler(self.bladebit_wrapper_logger_handler)
         self.bladebit_wrapper_logger.setLevel(level)
 
 
